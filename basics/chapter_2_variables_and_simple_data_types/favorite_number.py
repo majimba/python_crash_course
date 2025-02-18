6622f783359aebf96a50101afef569d12cefe76c
Chawana Maseka <chawana.maseka@gmail.com>
2025-02-18 13:55:19 +0200

    Add enhanced documentation to number programs with detailed explanations of Python basics

---
+++ basics/chapter_2_variables_and_simple_data_types/favorite_number.py
@@ -0,0 +1,12 @@
+# This program demonstrates the use of variables and f-strings in Python
+
+# Store favorite number in a variable
+# Variables can store any type of data (numbers, strings, etc.)
+# Here we store an integer (whole number)
+num = 8
+
+# Use an f-string to create a formatted message
+# f-strings (formatted string literals) let you embed expressions inside string literals
+# The {num} syntax inside the f-string will be replaced with the value of num
+# f-strings were introduced in Python 3.6 and are the preferred way to format strings
+print(f"My favorite number is {num}!")  # Output: My favorite number is 8!
---
+++ basics/chapter_2_variables_and_simple_data_types/number_8.py
@@ -0,0 +1,18 @@
+# This program demonstrates basic arithmetic operations in Python
+# Each operation will output the number 8 in different ways
+
+# Addition: Adding two numbers using the + operator
+# 3 + 5 evaluates to 8
+print(3 + 5)  # Output: 8
+
+# Subtraction: Subtracting one number from another using the - operator
+# 10 - 2 evaluates to 8
+print(10 - 2)  # Output: 8
+
+# Multiplication: Multiplying two numbers using the * operator
+# 2 * 4 evaluates to 8
+print(2 * 4)  # Output: 8
+
+# Division: Dividing one number by another using the / operator
+# 40 / 5 evaluates to 8.0 (note: division always returns a float in Python 3)
+print(40 / 5)  # Output: 8.0
